be745e4e30cc884014c07e69d44d10c4bcd0667b
gagana-07 <07.gaganasomashekar@gmailcom>
2024-11-06 19:52:18 +0530

    added

---
+++ day1.c/smallest 2 numbers.c
@@ -0,0 +1,14 @@
+//Find smallest of 2 numbers
+#include <stdio.h>
+int main() {
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    
+    if (num1 < num2) {
+        printf("The smallest number is %d.\n", num1);
+    } else {
+        printf("The smallest number is %d.\n", num2);
+    }
+    return 0;
+}
