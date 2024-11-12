64d4704e887256fb94532fe9886a9202b6a884f8
gagana-07 <07.gaganasomashekar@gmailcom>
2024-11-12 16:08:59 +0530

    added

---
+++ day9.java/02.java
@@ -0,0 +1,32 @@
+public class InsertionSort {
+    public InsertionSort(int[] arr) {
+    }
+    public static class insertionSort {
+        public insertionSort(int[] arr) {
+            for (int i = 1; i < arr.length; i++) {
+                int key = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j] > key) {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+        }
+        public static void main(String[] args) {
+            int[] arr = {12, 11, 13, 5, 6};
+            System.out.println("Array before sorting:");
+            printArray(arr);
+            insertionSort insertionSort = new insertionSort(arr);
+            System.out.println("Array after sorting:");
+            printArray(arr);
+        }
+        public static void printArray(int[] arr) {
+            for (int element : arr) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
---
+++ day9.java/03.java
@@ -0,0 +1,12 @@
+package linked;
+
+public class Linkedlist {
+    Node head;
+    public void add(int data) {
+        Node newNode = new Node(data);
+        newNode.next=head;
+        head=newNode;
+    }
+}
+
+
